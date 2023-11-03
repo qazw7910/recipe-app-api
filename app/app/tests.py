889f96ac420f643fcac283b56517d55040d3a6d2
qazw7910 <okmijn7980@gmail.com>
2023-11-04 05:15:39 +0800

    indent

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,7 +2,6 @@
 Simple tests
 """
 from django.test import SimpleTestCase
-
 from . import calc
 
 
@@ -14,7 +13,6 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-
 
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
